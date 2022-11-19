a1b8f6073e41de54d227881443ad6a049a56cf46
slungu <lungusorin.sl@gmail.com>
2022-11-20 01:48:17 +0200

    updated oidc controller

--- CreedHacks.Api/Controllers/OidcConfigurationController.cs
+++ CreedHacks.Api/Controllers/OidcConfigurationController.cs
@@ -7,24 +7,20 @@ namespace CreedHacks.Api.Controllers
     public class OidcConfigurationController : Controller
     {
         private readonly ILogger<OidcConfigurationController> _logger;
-        readonly IMetroRepository _sessionRepository;
 
         public OidcConfigurationController(
-            IMetroRepository sessionRepository,
             IClientRequestParametersProvider clientRequestParametersProvider,
             ILogger<OidcConfigurationController> logger)
         {
             ClientRequestParametersProvider = clientRequestParametersProvider;
-            _sessionRepository = sessionRepository;
             _logger = logger;
         }
 
         public IClientRequestParametersProvider ClientRequestParametersProvider { get; }
 
         [HttpGet("_configuration/{userId}")]
-        public IActionResult GetClientRequestParameters([FromRoute] int userId)
+        public IActionResult GetClientRequestParameters([FromRoute] string userId)
         {
-            var result = _sessionRepository.GetSessionAsync(userId);
             var parameters = ClientRequestParametersProvider.GetClientParameters(HttpContext, userId.ToString());
             return Ok(parameters);
         }
