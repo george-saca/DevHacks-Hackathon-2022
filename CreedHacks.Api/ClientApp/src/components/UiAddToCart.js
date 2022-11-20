cd1d3ea4d2f6195b92d225e0e9e341ddda32127b
dudis32 <iuliana.neagu32@gmail.com>
2022-11-20 03:38:45 +0200

    voice to text hook + redirect cart + add prd

--- CreedHacks.Api/ClientApp/src/components/UiAddToCart.js
+++ CreedHacks.Api/ClientApp/src/components/UiAddToCart.js
@@ -1,26 +1,56 @@
-import { TextField } from "@material-ui/core";
-import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
+import { useEffect } from "react";
+import { addToCart } from '../helpers/httpCaller';
+import useSpeechToText from 'react-hook-speech-to-text';
+import { useNavigate } from "react-router-dom";
 
 export const UiAddToCart = () => {
     const {
-        transcript,
-        listening,
-        resetTranscript,
-        browserSupportsSpeechRecognition
-      } = useSpeechRecognition();
+        error,
+        isRecording,
+        results,
+        startSpeechToText,
+        stopSpeechToText,
+      } = useSpeechToText({
+        continuous: true,
+        useLegacyResults: false,
+        speechRecognitionProperties: {
+            lang: 'en-US',
+            interimResults: true // Allows for displaying real-time speech results
+          }
+      });
+      
+      let history = useNavigate();
 
-      if (!browserSupportsSpeechRecognition) {
-        return <span>Browser doesn't support speech recognition.</span>;
-      }
+      useEffect(() => {
+        console.log(results);
+        sendAmountAndRedirectToCart(results[results.length-1]?.transcript);
+      }, [isRecording]);
+
+      if (error) return <p>Web Speech API is not available in this browser 🤷‍</p>;
+
+    const sendAmountAndRedirectToCart = async (transcriptNo) => {
+        
+        transcriptNo.match(/^\d+|\d+\b|\d+(?=\w)/g)
+           .map(function (v) {return +v;});
+
+        console.log(transcriptNo[0]);
+        await addToCart({
+            id: 1, 
+            img: "https://www.auchan.ro/public/images/h92/heb/h00/apa-plata-dorna-2-l-9309752459294.jpg", 
+            price: 6, 
+            title: "APA PLATA IZVORUL ALB",
+            amount: transcriptNo[0]});
+        
+            history("/");
+    }
 
   return (
     <div style={{ display: 'flex', flexDirection: 'column'}}>
-     <p>Microphone: {listening ? 'on' : 'off'}</p>
+     <h1>Recording: {isRecording.toString()}</h1>
      <h1 style={{ alignSelf: 'center' , textAlign:'center', display:'block'}}>Speak or Type to introduce the amount</h1>
      <br/>
-     <img style={{ alignSelf: 'center', display:'block', width:"200px"}} onClick={SpeechRecognition.startListening} src="https://cdn.icon-icons.com/icons2/2770/PNG/512/voice_microphone_icon_176686.png" />
+     <img style={{ alignSelf: 'center', display:'block', width:"200px"}} onClick={isRecording ? stopSpeechToText  : startSpeechToText} src="https://cdn.icon-icons.com/icons2/2770/PNG/512/voice_microphone_icon_176686.png" />
      <br/>
-     <TextField id="filled-basic" label="Amount" variant="filled" value={transcript}   type="number" style={{ alignSelf: 'center' , width:"100px", display:'block'}} InputLabelProps={{shrink: true}}/>
      <h2 style={{ alignSelf: 'center', display:'block' }}>Apa Plata Dorna Izvorul Alb</h2>
      <img  style={{ alignSelf: 'center', display:'block' }} class="flex-container" src="https://www.auchan.ro/public/images/h92/heb/h00/apa-plata-dorna-2-l-9309752459294.jpg"/>
      </div>
